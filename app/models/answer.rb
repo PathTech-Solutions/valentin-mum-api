068a254a30736bce64cb3ed05ebf62878eabeb88
Victor Gomez Lopez <victorgomez@MacBook-Air-de-Victor.local>
2024-03-28 13:51:50 +0100

    add reek, fix reek warning, add reek action, delete test folder

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -9,13 +9,13 @@ class Answer < ApplicationRecord
   private
 
   def score_within_range
-    if question.nil?
-      errors.add(:score, "question must be set")
+    unless question
+      errors.add(:answer, "question must be set")
       return
     end
     max_number = question.answers_number + 1
     return if score == max_number
 
-    errors.add(:score, "score must be #{max_number}")
+    errors.add(:answer, "score must be #{max_number}")
   end
 end
